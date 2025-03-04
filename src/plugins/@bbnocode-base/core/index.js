5ff3c25e0e1770188518807f415dae22a1912b83
阿逼 <bbnocode@outlook.com>
2025-03-04 17:27:09 +0800

    feat: 基础插件

--- src/plugins/@bbnocode-base/core/index.js
+++ src/plugins/@bbnocode-base/core/index.js
@@ -1,13 +1,46 @@
-
 /**
  * ENUM 转 像素
  * @param {String|Number} value
  * @returns {Number}
  */
 function ENUM_TO_PIXEL(value){
-  return Math.ceil(parseFloat(value) / 20 * 1.33445)
+  return Math.ceil(parseInt(value) / 20  * 1.33445)
 }
 
-export {
-  ENUM_TO_PIXEL
+
+/**
+ * 解析页面设置信息
+ */
+function parsePageSetting(docx){
+  // 获取 pgSz 元素
+  const sectPr = docx.officeDocument.content("w\\:sectPr")
+  const pgSz = sectPr.children("w\\:pgSz").get(0)
+  const pgMar = sectPr.children("w\\:pgMar").get(0)
+  
+  const pageWidth = ENUM_TO_PIXEL(pgSz.attribs["w:w"])
+  const pageHeight = ENUM_TO_PIXEL(pgSz.attribs["w:h"])
+  const pageMarginTop = ENUM_TO_PIXEL(pgMar.attribs["w:top"])
+  const pageMarginBottom = ENUM_TO_PIXEL(pgMar.attribs["w:bottom"])
+  const pageMarginLeft = ENUM_TO_PIXEL(pgMar.attribs["w:left"])
+  const pageMarginRight = ENUM_TO_PIXEL(pgMar.attribs["w:right"])
+  const pageHeader = ENUM_TO_PIXEL(pgMar.attribs["w:header"])
+  const pageGutter = ENUM_TO_PIXEL(pgMar.attribs["w:gutter"])
+
+  return {
+    width: pageWidth,
+    height: pageHeight,
+    marginTop: pageMarginTop,
+    marginBottom: pageMarginBottom,
+    marginLeft: pageMarginLeft,
+    marginRight: pageMarginRight,
+    header: pageHeader,
+    gutter: pageGutter
+  }
 }
+
+
+export {
+  ENUM_TO_PIXEL,
+  parsePageSetting
+}
+
--- src/plugins/@bbnocode-base/index.js
+++ src/plugins/@bbnocode-base/index.js
@@ -1,17 +1,22 @@
-import { ENUM_TO_PIXEL } from './core/index.js'
+import { ENUM_TO_PIXEL, parsePageSetting } from './core/index.js'
 
 export default {
   name:"@bbnocode/parse-base",
   priority:0,
   process:async(data)=>{
-    console.log("加载基础插件")
+    const pageInfo = parsePageSetting(data.document)
     const ctx = {
       ...data.ctx,
       ENUM_TO_PIXEL
     }
+    const content = {
+      ...data.content,
+      pageInfo
+    }
     return {
-      ...data,
-      ctx
+      document:data.document,
+      ctx,
+      content
     }
   }
 }
